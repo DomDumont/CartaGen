e61b48ea84835690001cf61f0d2f606061c01271
DomDom <dom.dumont@gmail.com>
2017-04-04 18:17:04 +0200

    Debut de build automatique linux (ça ne marchera pas)

---
+++ build_linux.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Stop at any error
+set -e
+
+# Location of the QT tools
+if [ -z ${QTDIR+x} ]; then
+	echo "QTDIR not defined- please set it to the location containing the Qt version to build against. For example:"
+	echo "  export QTDIR=~/Qt5.8.0/5.8/gcc_64"
+	exit 1
+fi
+
+QMAKE=${QTDIR}/bin/qmake
+MAKE=make
+LINUXDEPLOYQT=`pwd`/linuxdeployqt
+
+# Location of the source tree
+SOURCEDIR=`pwd`/Src
+
+# Location to build PatternPaint
+BUILDDIR='build-dist-linux'
+
+
+
+################## Build CartaGen ##########################
+mkdir -p ${BUILDDIR}
+pushd ${BUILDDIR}
+
+${QMAKE} ${SOURCEDIR}/CartaGen.pro \
+    -r \
+    -spec linux-g++
+
+#${MAKE} clean
+${MAKE} -j6
+
+popd
+
+
+
+################## Package using linuxdeployqt #################
+
+# pushd ${BUILDDIR}
+# pushd app
+
+# icns2png ${SOURCEDIR}/app/images/patternpaint.icns -x
+# cp patternpaint_256x256x32.png patternpaint.png
+
+# cp ${SOURCEDIR}/app/patternpaint.desktop ./
+
+# popd
+
+
+# TODO: this should be done automagically though the qt build tools?
+
+# mkdir -p app/lib
+# cp libblinky/libblinky.so.1 app/lib
+
+# unset LD_LIBRARY_PATH # Remove too old Qt from the search path; TODO: Move inside the linuxdeployqt AppImage
+
+# PATH=${QTDIR}/bin:${PATH} ${LINUXDEPLOYQT} app/PatternPaint -bundle-non-qt-libs
+# PATH=${QTDIR}/bin:${PATH} ${LINUXDEPLOYQT} app/PatternPaint -appimage
+
+# tar -cjf PatternPaint-x86_64_${VERSION}.tar.bz2 PatternPaint-x86_64.AppImage
+
+# popd
