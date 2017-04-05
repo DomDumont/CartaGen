3bc83fa2b9ad5db6eefa370e2bee20b737aa065c
Dominique Dumont <dom.dumont@gmail.com>
2017-04-05 11:58:50 +0200

    Add butler download for travis linux and osx

--- Scripts/ci/linux/install.sh
+++ Scripts/ci/linux/install.sh
@@ -1,7 +1,10 @@
 
- sudo apt-get -y install qt58base qt58declarative qt58serialport
+sudo apt-get -y install qt58base qt58declarative qt58serialport
 
- wget -c https://github.com/probonopd/linuxdeployqt/releases/download/2/linuxdeployqt-2-x86_64.AppImage -O linuxdeployqt
- chmod a+x linuxdeployqt
+wget https://dl.itch.ovh/butler/linux-amd64/head/butler
+chmod +x butler
 
- export QTDIR=/opt/qt58
+wget -c https://github.com/probonopd/linuxdeployqt/releases/download/2/linuxdeployqt-2-x86_64.AppImage -O linuxdeployqt
+chmod a+x linuxdeployqt
+
+export QTDIR=/opt/qt58
---
+++ Scripts/ci/osx/install.sh
@@ -0,0 +1,9 @@
+
+wget https://dl.itch.ovh/butler/linux-amd64/head/butler
+chmod +x butler
+
+brew update
+brew install qt5
+chmod -R 755 /usr/local/opt/qt5/*
+
+export QTDIR=/usr/local/opt/qt5
